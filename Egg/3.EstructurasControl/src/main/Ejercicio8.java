eebf5e97c429d06738436756fc50f16a7834886a
Diego_d4c <diego_cantero@outlook.es>
2025-03-06 18:17:02 -0300

    estructuras de control

---
+++ Egg/3.EstructurasControl/src/main/Ejercicio8.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package main;
+
+import java.util.Scanner;
+
+/**
+ *Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. 
+ * Por ejemplo, si el cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
+* * * *
+*      *
+*      *
+* * * *
+
+ * @author kroban_d4c
+ */
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        
+        int num = pedirNum(input);
+        dibujar(num);
+        
+
+    }
+    //2 bucles for que hagan asteriscos en base a 0 y N?
+    public static void dibujar(int n){
+                for(int i = 1; i <= n ; i++){
+                        for(int j = 1; j <= n ; j++){
+                            if(i == 1 || i == n || j == 1 || j == n){
+                                    System.out.print("*");
+                            }else{
+                                    System.out.print(" ");
+                }
+                        }
+                        
+                    System.out.println("");
+        }
+    }
+    
+    public static int pedirNum(Scanner input){
+        System.out.println("Ingrese un numero");
+        int num = input.nextInt();
+        
+        return num;
+    }
+}
---
+++ Egg/3.EstructurasControl/src/main/EjerciciosX2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package main;
+
+/**
+ *Declarar cuatro variables de tipo entero A, B, C y D y asignarle un valor diferente a cada una.
+ * A continuación, realizar las instrucciones necesarias para que: 
+ * B tome el valor de C, C tome el valor de A, A tome el valor de D y D tome el valor de B. 
+ * Mostrar los valores iniciales y los valores finales de cada variable. 
+ * Utilizar sólo una variable auxiliar.
+ * 
+ * @author kroban_d4c
+ */
+public class EjerciciosX2 {
+
+}
